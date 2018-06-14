3161a979fcdce0b1f1fcc92b654c26a52ba3d6c9
Carl <c.almayda09@gmail.com>
2018-06-14 17:08:39 +0800

    Initial commit

---
+++ assets/parts/modals.php
@@ -0,0 +1,73 @@
+<div class="modal fade" id="createuser" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Create an avatar.</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col-md-12">
+                    <div class="row">
+                        <br/>
+                        <div class="col-md-1">
+
+                        </div>
+                        <div class="col-md-10">
+                            <small class="modal-title">You can create your own character on this world. You can be unique or you may see a doppelganger.</small>
+                            <form>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Username / Email</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username or email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Confirm Password</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your character's name">
+                                    <small id="emailHelp" class="form-text text-muted">This name will appear as your display name</small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Nickname</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your character's name">
+                                    <small id="emailHelp" class="form-text text-muted">This name will appear as your display name</small>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <div class="form-check">
+                                                <input class="form-check-input" name="gender" type="radio" value="" id="defaultCheck1">
+                                                <label class="form-check-label" for="defaultCheck1">
+                                                    Male
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input class="form-check-input" name="gender" type="radio" value="" id="defaultCheck2">
+                                                <label class="form-check-label" for="defaultCheck2">
+                                                    Female
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br/>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Create Game Character</button>
+                                <small id="emailHelp" class="form-text text-muted">You can customize profile avatar later after you create a game character.</small>
+                            </form>
+                        </div>
+                        <div class="col-md-1">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
